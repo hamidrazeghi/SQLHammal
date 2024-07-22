fef7cff6c57d28cdd8734349d22af6583bfb3749
Hamid Razeghi <h.razeghi@asax.local>
2024-07-22 15:38:52 +0330

    add project withj menus

--- SqlHammal/SqlHammal/Program.cs
+++ SqlHammal/SqlHammal/Program.cs
@@ -1,8 +1,187 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Spectre.Console;
+using System.ComponentModel.Design;
+using System.Data.SqlClient;
+using System.Data;
 using System.Runtime.CompilerServices;
+using SqlHammal;
 
-Console.WriteLine("Hello, World!");
 
+
+string? sourceConnectionString;
+string? targetConnectionString;
+string selectTop = "";
+
+// Adjust Configuration
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.Sources.Clear();
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    })
+    .Build();
+var configuration = host.Services.GetService(typeof(IConfiguration)) as IConfiguration;
+
+sourceConnectionString = configuration["ConnectionStrings:Source"];
+targetConnectionString = configuration["ConnectionStrings:Target"];
+
+if (string.IsNullOrWhiteSpace(sourceConnectionString) || string.IsNullOrWhiteSpace(targetConnectionString))
+    AnsiConsole.WriteLine("[red] Please insert connection string in appsetting.js and re-run the appliation");
+else
+{
+
+    Menu();
+    Console.ReadKey();
+}
+
+
+
+
+
+void Menu()
+{
+
+    AnsiConsole.Write(
+             new FigletText("SQL - Hammal")
+            .LeftJustified()
+            .Color(Color.Red));
+
+
+
+    var copyType = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("Select amount of data to copy ")
+                        .PageSize(10)
+                        .AddChoices(new[] {
+                            "Copy Top Number ", "Copy All Data "
+                        }));
+
+    if (copyType == "Copy Top Number ")
+    {
+        var topCount = getTopCountNumber();
+        selectTop = $" SELECT {topCount} ";
+    }
+
+    Run();
+
+}
+void Run()
+{
+
+    try
+    {
+
+        using (SqlConnection sourceConnection = new SqlConnection(sourceConnectionString))
+        using (SqlConnection targetConnection = new SqlConnection(targetConnectionString))
+        {
+            sourceConnection.Open();
+            targetConnection.Open();
+
+            var schemaNames = Sqlhandler.GetSchemaNames(sourceConnection);
+            var tableOrder = Sqlhandler.GetTableOrder(sourceConnection, schemaNames);
+
+            int tableCounts = 0;
+            int totalTableCounts = tableOrder.Count;
+
+            foreach (var fullTableName in tableOrder)
+            {
+                string[] tableParts = fullTableName.Split('.');
+                string schemaName = tableParts[0];
+                string tableName = tableParts[1];
+                Console.WriteLine($"Proccessing Table => {schemaName}.{tableName} ");
+
+
+                DataTable schemaTable = new DataTable();
+                string schemaQuery = $@"
+                    SELECT COLUMN_NAME,DATA_TYPE, COLUMNPROPERTY(object_id(TABLE_SCHEMA + '.' + TABLE_NAME), COLUMN_NAME, 'IsIdentity') AS IsIdentity
+                    FROM INFORMATION_SCHEMA.COLUMNS
+                    WHERE TABLE_NAME = '{tableName}' AND TABLE_SCHEMA = '{schemaName}'";
+                using (SqlCommand schemaCommand = new SqlCommand(schemaQuery, sourceConnection))
+                using (SqlDataAdapter schemaAdapter = new SqlDataAdapter(schemaCommand))
+                {
+                    schemaAdapter.Fill(schemaTable);
+                }
+
+                bool hasIdentityColumn = schemaTable.AsEnumerable().Any(row => row.Field<int>("IsIdentity") == 1);
+
+                string[] columnNames = schemaTable.AsEnumerable().Select(r => r.Field<string>("COLUMN_NAME")).ToArray();
+                string columnList = string.Join(", ", columnNames);
+                string parameterList = string.Join(", ", columnNames.Select((name, index) => $"@p{index}"));
+
+
+
+                string identityInsertOn = hasIdentityColumn ? $"SET IDENTITY_INSERT {schemaName}.{tableName} ON; " : "";
+                string identityInsertOff = hasIdentityColumn ? $" SET IDENTITY_INSERT {schemaName}.{tableName} OFF;" : "";
+                string insertQuery = $"{identityInsertOn}INSERT INTO {schemaName}.{tableName} ({columnList}) VALUES ({parameterList});{identityInsertOff}";
+
+                using (SqlCommand insertCommand = new SqlCommand(insertQuery, targetConnection))
+                {
+                    for (int i = 0; i < columnNames.Length; i++)
+                    {
+                        string columnName = columnNames[i];
+                        string dataType = schemaTable.AsEnumerable().First(row => row.Field<string>("COLUMN_NAME") == columnName).Field<string>("DATA_TYPE");
+
+                        SqlDbType sqlDbType = Sqlhandler.GetSqlDbType(dataType);
+
+                        insertCommand.Parameters.Add(new SqlParameter($"@p{i}", sqlDbType));
+                    }
+
+
+                    string selectQuery = $"SELECT {selectTop} * FROM {schemaName}.{tableName} ORDER BY 1 DESC";
+                    using (SqlCommand selectCommand = new SqlCommand(selectQuery, sourceConnection))
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < columnNames.Length; i++)
+                            {
+                                insertCommand.Parameters[$"@p{i}"].Value = reader[columnNames[i]];
+                            }
+                            try
+                            {
+                                insertCommand.ExecuteNonQuery();
+                            }
+                            catch
+                            {
+                                continue;
+                            }
+
+                        }
+                    }
+                }
+                Console.WriteLine($"Table Has been Proccessd =>  {schemaName}.{tableName} ");
+                tableCounts++;
+            }
+
+
+            Console.WriteLine($" --- {tableCounts} Proccessed from {totalTableCounts}");
+            Console.WriteLine("All Done !");
+        }
+
+    }
+    catch (Exception exp)
+    {
+        Console.WriteLine(exp.Message);
+        throw;
+    }
+}
+
+
+
+int getTopCountNumber()
+{
+    AnsiConsole.WriteLine("Enter the number of records to fetch from each table : ");
+    var topCount = Console.ReadLine();
+
+    if (int.TryParse(topCount, out int number))
+        return number;
+    else
+        getTopCountNumber();
+
+
+    return 0;
+}
 
 
 
---
+++ SqlHammal/SqlHammal/SqlHandler.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlHammal
+{
+    public static class Sqlhandler
+    {
+        public static List<string> GetTableOrder(SqlConnection connection, Dictionary<int, string> schemaNames)
+        {
+            var tableOrder = new List<string>();
+            var dependencies = new Dictionary<string, List<string>>();
+
+            // Get all tables and their foreign key dependencies
+            string query = @"
+            SELECT 
+                fk.name AS FK_Name, 
+                tp.name AS ParentTable, 
+                tr.name AS ReferencedTable,
+                tp.schema_id AS ParentSchemaId,
+                tr.schema_id AS ReferencedSchemaId
+            FROM 
+                sys.foreign_keys AS fk
+                INNER JOIN sys.tables AS tp ON fk.parent_object_id = tp.object_id
+                INNER JOIN sys.tables AS tr ON fk.referenced_object_id = tr.object_id
+                INNER JOIN sys.schemas AS sp ON tp.schema_id = sp.schema_id
+                INNER JOIN sys.schemas AS sr ON tr.schema_id = sr.schema_id";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    string parentTable = reader["ParentTable"].ToString();
+                    string referencedTable = reader["ReferencedTable"].ToString();
+                    string parentSchema = schemaNames[(int)reader["ParentSchemaId"]];
+                    string referencedSchema = schemaNames[(int)reader["ReferencedSchemaId"]];
+
+                    string parentFullTableName = $"{parentSchema}.{parentTable}";
+                    string referencedFullTableName = $"{referencedSchema}.{referencedTable}";
+
+                    if (!dependencies.ContainsKey(parentFullTableName))
+                    {
+                        dependencies[parentFullTableName] = new List<string>();
+                    }
+
+                    dependencies[parentFullTableName].Add(referencedFullTableName);
+                }
+            }
+
+
+            var visited = new HashSet<string>();
+            foreach (var table in dependencies.Keys)
+            {
+                Visit(table, dependencies, visited, tableOrder);
+            }
+
+
+
+            var allTables = new HashSet<string>();
+            string allTablesQuery = @"
+            SELECT 
+                TABLE_SCHEMA, 
+                TABLE_NAME 
+            FROM 
+                INFORMATION_SCHEMA.TABLES 
+            WHERE 
+                TABLE_TYPE = 'BASE TABLE'";
+            using (SqlCommand command = new SqlCommand(allTablesQuery, connection))
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    string schemaName = reader["TABLE_SCHEMA"].ToString();
+                    string tableName = reader["TABLE_NAME"].ToString();
+                    allTables.Add($"{schemaName}.{tableName}");
+                }
+            }
+
+
+            foreach (var table in allTables.Except(tableOrder))
+            {
+                tableOrder.Add(table);
+            }
+
+
+
+            return tableOrder;
+        }
+
+        public static SqlDbType GetSqlDbType(string dataType)
+        {
+            switch (dataType.ToLower())
+            {
+                case "bigint": return SqlDbType.BigInt;
+                case "binary": return SqlDbType.Binary;
+                case "bit": return SqlDbType.Bit;
+                case "char": return SqlDbType.Char;
+                case "date": return SqlDbType.Date;
+                case "datetime": return SqlDbType.DateTime;
+                case "datetime2": return SqlDbType.DateTime2;
+                case "datetimeoffset": return SqlDbType.DateTimeOffset;
+                case "decimal": return SqlDbType.Decimal;
+                case "float": return SqlDbType.Float;
+                case "image": return SqlDbType.Image;
+                case "int": return SqlDbType.Int;
+                case "money": return SqlDbType.Money;
+                case "nchar": return SqlDbType.NChar;
+                case "ntext": return SqlDbType.NText;
+                case "nvarchar": return SqlDbType.NVarChar;
+                case "real": return SqlDbType.Real;
+                case "smalldatetime": return SqlDbType.SmallDateTime;
+                case "smallint": return SqlDbType.SmallInt;
+                case "smallmoney": return SqlDbType.SmallMoney;
+                case "text": return SqlDbType.Text;
+                case "time": return SqlDbType.Time;
+                case "timestamp": return SqlDbType.Timestamp;
+                case "tinyint": return SqlDbType.TinyInt;
+                case "uniqueidentifier": return SqlDbType.UniqueIdentifier;
+                case "varbinary": return SqlDbType.VarBinary;
+                case "varchar": return SqlDbType.VarChar;
+                case "xml": return SqlDbType.Xml;
+                default: throw new ArgumentOutOfRangeException(nameof(dataType), $"Unsupported SQL data type: {dataType}");
+            }
+        }
+
+        public static Dictionary<int, string> GetSchemaNames(SqlConnection connection)
+        {
+            var schemaNames = new Dictionary<int, string>();
+            string query = "SELECT schema_id, name FROM sys.schemas";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    schemaNames.Add(reader.GetInt32(0), reader.GetString(1));
+                }
+            }
+            return schemaNames;
+        }
+
+        public static void Visit(string table, Dictionary<string, List<string>> dependencies, HashSet<string> visited, List<string> tableOrder)
+        {
+            if (!visited.Contains(table))
+            {
+                visited.Add(table);
+
+                if (dependencies.ContainsKey(table))
+                {
+                    foreach (var dependency in dependencies[table])
+                    {
+                        Visit(dependency, dependencies, visited, tableOrder);
+                    }
+                }
+
+                tableOrder.Add(table);
+            }
+        }
+    }
+}
